b2f8a86c86a3daf3e4ecbbfe3a6a1f6e49dcd5bc
Aline <alinemokfa@hotmail.co.uk>
2018-08-17 09:17:25 +0100

    Seed and display posts

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Aline M',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '16 Aug 2018'
+    },
+    {
+        'author': 'Patrick C',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '17 Aug 2018'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
