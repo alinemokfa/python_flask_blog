ef6c24dacfe39de5e05582cf18fe10e8aed8182d
Aline <alinemokfa@hotmail.co.uk>
2018-08-17 17:08:08 +0100

    Refactor

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ def about():
 
 @app.route("/register")
 def register():
-    from = RegistrationForm()
+    form = RegistrationForm()
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
 def login():
-    from = LoginForm()
+    form = LoginForm()
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
