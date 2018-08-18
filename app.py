0dfb49a9495a48936312a3a9bc0a9bc2536f15c3
Aline <alinemokfa@hotmail.co.uk>
2018-08-18 22:23:32 +0100

    Add User db model

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 # Below key is only an example. This key will be replaced by an env. variable once deployed.
 app.config['SECRET_KEY'] = '94557b91dc00450b6b32460246fcef8f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+#TODO Seeding to be deleted
 posts = [
     {
         'author': 'Aline M',
@@ -37,9 +51,16 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        #TODO Seeding to be deleted once db is up
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You are now logged in!', 'sucess')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
