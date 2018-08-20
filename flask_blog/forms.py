2c916d3329bfe850c06306c2942cbf974569b78a
Aline <alinemokfa@hotmail.co.uk>
2018-08-20 11:20:57 +0100

    Add custom validators

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_blog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,6 +13,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Pasword',
                                       validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
