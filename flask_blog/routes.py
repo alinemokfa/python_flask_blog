c22074a97210f94bd212be1da9d9027b741e5448
Aline <alinemokfa@hotmail.co.uk>
2018-08-20 13:11:42 +0100

    Redirect user to home if authenticated

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flask_blog import app, db, bcrypt
 from flask_blog.forms import RegistrationForm, LoginForm
 from flask_blog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user
 
 posts = [
     {
@@ -30,6 +30,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -42,6 +44,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
