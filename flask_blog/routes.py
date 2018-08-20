de783c0e7d7eeeae32824b58151671496147897f
Aline <alinemokfa@hotmail.co.uk>
2018-08-20 23:17:51 +0100

    Repopulate details

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -72,5 +72,8 @@ def account():
         db.session.commit()
         flash('Your account has ben updated!', 'success')
         return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
