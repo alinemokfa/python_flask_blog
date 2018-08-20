59ad95d40d627b2ef760311aa90a28db710382ec
Aline <alinemokfa@hotmail.co.uk>
2018-08-20 12:18:57 +0100

    Add UserMixin to manage sessions

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # Below key is only an example. This key will be replaced by an env. variable once deployed.
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = '94557b91dc00450b6b32460246fcef8f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_blog import routes
