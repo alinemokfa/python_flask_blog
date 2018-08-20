667ff83d52dca54609073f512101d163cea8ae72
Aline <alinemokfa@hotmail.co.uk>
2018-08-20 10:56:52 +0100

    Generate hashed password and add user to db

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 # Below key is only an example. This key will be replaced by an env. variable once deployed.
 app.config['SECRET_KEY'] = '94557b91dc00450b6b32460246fcef8f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
