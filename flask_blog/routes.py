9d58534d7950caeab9c23cc85aabf19a86b255c7
Aline <alinemokfa@hotmail.co.uk>
2018-08-22 17:53:56 +0100

    Save picture to the file system

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 from flask import render_template, url_for, flash, redirect, request
 from flask_blog import app, db, bcrypt
@@ -65,14 +66,18 @@ def logout():
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
-
+    # f_name refactored to _ (as it's a variable that's not going to be used)
+    _, f_ext = os.path.splittext(form_picture.filename)
+    picture_fn = random_hex + f_text
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        # if form.picture.data:
+        if form.picture.data:
 
         current_user.username = form.username.data
         current_user.email = form.email.data
