9c5100d3dc09a1c024d9a9e047d963461364a83a
Aline <alinemokfa@hotmail.co.uk>
2018-08-22 16:31:28 +0100

    Corrected spelling mistake

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from falsk_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_blog.models import User
