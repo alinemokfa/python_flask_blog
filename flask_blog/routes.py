d44ada168962e387b5e2a889f182965b0e3e0ff7
Aline <alinemokfa@hotmail.co.uk>
2018-08-22 17:59:11 +0100

    Correct typos

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -67,7 +67,7 @@ def logout():
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     # f_name refactored to _ (as it's a variable that's not going to be used)
-    _, f_ext = os.path.splittext(form_picture.filename)
+    _, f_text = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_text
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
     form_picture.save(picture_path)
